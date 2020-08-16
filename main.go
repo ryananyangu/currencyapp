61d8f0be9cd68ff29dd392fd7634dd773cab374d
ryananyangu <ryananyangu@gmail.com>
2020-08-16 09:13:45 +0300

    Function to get the currency file and convert into list of strings complete

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+var currenciesURL = "https://focusmobile-interview-materials.s3.eu-west-3.amazonaws.com/Cheap.Stocks.Internationalization.Currencies.csv"
+
+func getCurrencies(url string) ([]string, error) {
+	// Make a http request to ge the currencies
+	client := &http.Client{}
+
+	req, _ := http.NewRequest("GET", url, nil)
+	resp, err := client.Do(req)
+	if err != nil {
+		// Return an empty list and the error if we've got any
+		return nil, err
+	}
+	respBody, _ := ioutil.ReadAll(resp.Body)
+	// Convert the body to a string and split by new line
+	csvLines := strings.Split(string(respBody), "\r\n")
+
+	return csvLines, nil
+}
+
 func main() {
 
 }
